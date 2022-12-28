708789dd97e37498cc2103bfefaead05ef91991b
Diego Balderrama <diegowrhasta@gmail.com>
2022-12-27 20:48:34 -0400

    Washing machine

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using DummyLibrary.Classes;
+
+var washingMachine = new WashingMachine();
+
+// washingMachine.UpdateClothes(); Is wrong because the access level is private, you can't access it unles from the 
+// class itself. That encapsulates that functionality, and also washingMachine._numberOfClothes is illegal, it is 
+// a private field, and cannot be accessed from the outside.
+washingMachine.WashClothes();
+
+// Encapsulation is this ability/property of knowing what to expose to the outside and what not to, more often than 
+// not in order to keep a class working consistently and correctly.
---
+++ DummyLibrary/Classes/WashingMachine.cs
@@ -0,0 +1,17 @@
+﻿namespace DummyLibrary.Classes;
+
+public class WashingMachine
+{
+    private int _numberOfClothes;
+
+    public void WashClothes()
+    {
+        UpdateClothes();
+        // Do more washing logic here.
+    }
+    
+    private void UpdateClothes()
+    {
+        _numberOfClothes++;
+    }
+}
